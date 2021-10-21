af7bc84caa4e54d9cf435de7dcc808f4e6333f89
utylee <utylee@gmail.com>
2021-10-21 23:56:40 +0900

    netstart persistent to yes for always run

--- netstart.bat
+++ netstart.bat
@@ -1,3 +1,3 @@
 set USER=utylee
 set PASSWD=sksmsqnwk11
-net use U: \\192.168.0.201\clark %PASSWD% /user:%USER% /persistent:no
+net use U: \\192.168.0.201\clark %PASSWD% /user:%USER% /persistent:yes
