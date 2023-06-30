7933bf582ea0c7ef3ad5ecf1964c212c6c54a5a0
utylee <utylee@gmail.com>
2023-06-30 20:47:47 +0900

    chg: wsl2-network nothing different but comment added

--- wsl2-network.ps1
+++ wsl2-network.ps1
@@ -4,6 +4,10 @@
 # 언제부턴가 내부 ip를 인식하지 못하는 것인지 포워딩후에도 계속 접속이 되지 않아
 # $USER/.wslconfig 의 localhostForwarding 을 true로 준 후 아래 포워딩을
 # 내부 아이피 직접이 아닌 localhost 로 지정하여 문제를 해결하였습니다 
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# ### ===> wsl, wsl2 를 동시에 사용하면서 default가 wsl1으로 설정되어 생긴 문제였습니다
+# wsl1의 bash.exe 이다 보니 내부 wsl2 ip가 아닌 192.168.1.204 를 가져와서
+# 포워딩이 전혀 안되고 있는 꼴이었습니다
 $remoteport = bash.exe -c "ifconfig eth0 | grep 'inet '"
 $found = $remoteport -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
 
@@ -44,9 +48,9 @@ iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound
 for( $i = 0; $i -lt $ports.length; $i++ ){
   $port = $ports[$i];
   iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
-  iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=localhost";
+  # iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=localhost";
   # 내부 아이피로 포워딩을 못해주는 문제 발생. 
-  # iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
+  iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
 
   # iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr0";
   # iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr0 connectport=$port connectaddress=$remoteport";
